7c85bf6d9645397ebd39e95cceedb2c7aefc649a
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2023-02-01 21:41:29 +0900

    RSpecをセットアップ
    bin/rails generate rspec:install
    bundle binstubs rspec-core

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,7 @@ module Tempra
       g.helper false
       g.javascripts false
       g.stylesheets false
+      g.test_framework :rspec, controller_specs: false, request_specs: false, routing_specs: false, view_specs: false
     end
   end
 end
