ea5a96c5b8c813ece51e4deb2adfcb253ad9a19a
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2023-03-21 10:51:51 +0900

    Deviseによる認証系ページのパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :administrators
+  devise_for :users, path: "user", path_names: { sign_in: "login", sign_out: "logout" }
+  devise_for :administrators, path: "kanri", path_names: { sign_in: "login", sign_out: "logout" }
 
   root to: "top#index"
 
