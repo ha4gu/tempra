811077213ab31e976c5e56d648c297be222a3501
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2023-03-21 20:53:45 +0900

    Font Awesomeを導入
    bin/importmap pin @fortawesome/fontawesome-free @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons --from unpkg

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@fortawesome/fontawesome-free", to: "https://unpkg.com/@fortawesome/fontawesome-free@6.3.0/js/all.js"
+pin "@fortawesome/fontawesome-svg-core", to: "https://unpkg.com/@fortawesome/fontawesome-svg-core@6.3.0/index.mjs"
+pin "@fortawesome/free-brands-svg-icons", to: "https://unpkg.com/@fortawesome/free-brands-svg-icons@6.3.0/index.mjs"
+pin "@fortawesome/free-regular-svg-icons", to: "https://unpkg.com/@fortawesome/free-regular-svg-icons@6.3.0/index.mjs"
+pin "@fortawesome/free-solid-svg-icons", to: "https://unpkg.com/@fortawesome/free-solid-svg-icons@6.3.0/index.mjs"
